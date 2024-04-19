d7969c9be1d9b7743e9dc491c0ff3c819681c467
Kundan <techzombie333@gmail.com>
2024-04-20 02:32:50 +0530

    final commit

--- smartContract/hardhat.config.js
+++ smartContract/hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.18",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/pk1YF6zqa-Ze2cMoqQI59umIgTeWFR47",
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
